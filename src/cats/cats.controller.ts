61429587b37b63ad3c960be57166580b5d634ce5
gaisdav <gais.davletkildin@gmail.com>
2023-04-16 12:56:55 +0200

    added auth

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -7,35 +7,38 @@ import {
   ParseIntPipe,
   Patch,
   Post,
-  UsePipes,
-  ValidationPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatDto } from './dto/cat.dto';
 import { CreateCatDto } from './dto/create.dto';
 import { UpdateCatDto } from './dto/update.dto';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
+  @UseGuards(AuthGuard)
   async getAllCats(): Promise<CatDto[]> {
     return await this.catsService.getAllCats();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   async getOneCat(@Param('id', ParseIntPipe) id: number): Promise<CatDto> {
     return await this.catsService.getOneCat(id);
   }
 
   @Post()
+  @UseGuards(AuthGuard)
   async createCat(@Body() dto: CreateCatDto): Promise<CatDto> {
     return await this.catsService.createCat(dto);
   }
 
   @Patch(':id')
-  @UsePipes(new ValidationPipe())
+  @UseGuards(AuthGuard)
   async updateCat(
     @Body() dto: UpdateCatDto,
     @Param('id', ParseIntPipe) id: number,
@@ -44,6 +47,7 @@ export class CatsController {
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
   async deleteCat(@Param('id', ParseIntPipe) id: number): Promise<void> {
     return await this.catsService.deleteCat(id);
   }
